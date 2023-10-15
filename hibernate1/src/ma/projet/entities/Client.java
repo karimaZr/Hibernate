0d8c9011bf7d2eda6d1d664683e9d97607545fcf
karimaZr <128175856+karimaZr@users.noreply.github.com>
2023-10-15 22:40:26 +0000

    Initial commit

---
+++ hibernate1/src/ma/projet/entities/Client.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ma.projet.entities;
+
+import java.util.Date;
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Temporal;
+
+@Entity
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String nom;
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date date;
+    
+    @OneToMany (mappedBy = "client", fetch = FetchType.EAGER)
+    private List<Commande> commandes;
+
+    public Client() {
+    }
+
+    public Client(String nom, Date date) {
+        this.nom = nom;
+        this.date = date;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public List<Commande> getCommandes() {
+        return commandes;
+    }
+
+    public void setCommandes(List<Commande> commandes) {
+        this.commandes = commandes;
+    }
+    
+    
+}
---
+++ hibernate1/src/ma/projet/entities/Commande.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ma.projet.entities;
+
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+
+/**
+ *
+ * @author Lachgar
+ */
+@Entity
+public class Commande {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String code;
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date date;
+    @ManyToOne
+    private Client client;
+
+    public Commande() {
+    }
+
+    public Commande(String code, Date date, Client client) {
+        this.code = code;
+        this.date = date;
+        this.client = client;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+    
+    
+    
+}
